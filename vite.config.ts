88926a970fabc72e8e732312eec94b270757e124
vini <viniciuscalebebarbosa@gmail.com>
2025-03-17 11:43:03 -0300

    permiti acesso ao render

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,14 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true, // Permite acesso externo
     port: 8080,
+    strictPort: true,
+    allowedHosts: ["site-escritorio-de-advocacia.onrender.com"], // Permite acesso no Render
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
